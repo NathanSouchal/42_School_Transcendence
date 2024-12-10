802e63d847711b079fe96679a8f90a32b133604f
Timo Nicolaux <tnicolau@z3r12p2.42lyon.fr>
2024-12-10 10:11:16 +0100

    added comments on backend

--- backend/project/api/serializers.py
+++ backend/project/api/serializers.py
@@ -10,3 +10,22 @@ class UserSerializer(serializers.ModelSerializer):
 	def create(self, validated_data):
 		user = User.objects.create_user(**validated_data)
 		return user
+
+"""
+	serializer: convertit des objets Python (ici des instances de modeles) en format JSON ou autre
+
+	fields = ['id', 'username', 'password'] -> on selectionne les champs du modele User qui seront
+	inclus dans la representation JSON
+
+	extra_kwargs = {'password': {'write_only': True}} -> ici on personnalise le comportement du champ
+	password, on le passe en ecriture seule. Il peut être transmis dans une requête login par exemple
+	mais il ne sera jamais retourné dans la réponse JSON (on le protege d'etre expose)
+
+	create() est la methode appelee pour creer un nouvel objet utilisateur (POST ou PUT sur un User
+	arrive ici pour sauvegarder les donnees). **validated_data est un déballage (unpacking) du dictionnaire
+	validated_data qui contient par exemple {username='john', password='securepassword'}
+	User.objects.create_user(**validated_data) -> User.UserManager().create_user
+
+	La méthode par défaut de DRF pour create ne sait pas comment gérer les mots de passe (les hacher)
+	En remplaçant cette méthode, on personnalise la manière dont un utilisateur est créé.
+"""
