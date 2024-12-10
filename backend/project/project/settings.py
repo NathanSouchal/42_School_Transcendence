5e8ccbf26416f6244e59c4914f9af96d6d66b6a5
Nathan Souchal <nsouchal@z3r12p4.42lyon.fr>
2024-12-10 16:13:18 +0100

    adding postgres container but error whith the volume's path

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,10 +87,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB', default='default_db_name'),
+        'USER': config('POSTGRES_USER', default='default_user'),
+        'PASSWORD': config('POSTGRES_PASSWORD', default='default_password'),
+        'HOST': config('POSTGRES_HOST', default='localhost'),
+        'PORT': config('POSTGRES_PORT', default=5432, cast=int),
     }
 }
+
 
 
 # Password validation
