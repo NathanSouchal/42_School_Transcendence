e56bde67467ef49d84b9c2c67007a93da6799184
cefue <cesar.fuentes@outlook.fr>
2025-01-27 14:29:25 +0100

    small changes to include django channels

--- backend/project/project/asgi.py
+++ backend/project/project/asgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+from game.routing import websocket_urlpatterns
 
-# application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
@@ -23,7 +24,7 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
 
