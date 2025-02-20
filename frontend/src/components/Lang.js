779bd98d15f4f00ecff19c6952b5d7cfbc877488
Timo Nicolaux <tnicolau@z3r12p4.42lyon.fr>
2025-02-20 16:07:42 +0100

    working

--- frontend/src/components/Lang.js
+++ frontend/src/components/Lang.js
@@ -9,49 +9,44 @@ export class Lang {
     const dropdownBtn = document.getElementById("lang-dropdown-btn");
     const langMenu = document.getElementById("lang-menu");
 
-    if (!langDiv || !dropdownBtn || !langMenu) {
-      console.error(
-        "Les éléments de la langue ne sont pas trouvés dans le DOM !"
-      );
-      return;
-    }
-
-    dropdownBtn.addEventListener("click", (e) => {
-      langDiv.classList.toggle("open");
-      e.stopPropagation();
-    });
-
-    const selectedLangImg = document.getElementById("selected-lang-img");
-    if (selectedLangImg) {
-      if (this.state.state.lang === "EN") selectedLangImg.src = "english.jpg";
-      else if (this.state.state.lang === "FR")
-        selectedLangImg.src = "french.jpg";
-    }
-
-    // Sélection d'une langue
-    document.querySelectorAll(".lang-menu li").forEach((item) => {
-      item.addEventListener("click", (e) => {
-        const newLang = e.currentTarget.getAttribute("data-lang");
-        const langText =
-          e.currentTarget.querySelector(".lang-name").textContent;
-        this.state.updateLang(langText);
-        console.log("this.state.state.lang : " + this.state.state.lang);
-        langDiv.classList.remove("open");
-
-        selectedLangImg.classList.add("fade-out");
-
-        setTimeout(() => {
-          selectedLangImg.src = newLang;
-          selectedLangImg.classList.remove("fade-out");
-        }, 250);
+    if (langDiv && dropdownBtn && langMenu) {
+      dropdownBtn.addEventListener("click", (e) => {
+        langDiv.classList.toggle("open");
+        e.stopPropagation();
       });
-    });
 
-    // Fermer le menu quand on clique ailleurs
-    document.addEventListener("click", (e) => {
-      if (!langDiv.contains(e.target)) {
-        langDiv.classList.remove("open");
+      const selectedLangImg = document.getElementById("selected-lang-img");
+      if (selectedLangImg) {
+        if (this.state.state.lang === "EN") selectedLangImg.src = "english.jpg";
+        else if (this.state.state.lang === "FR")
+          selectedLangImg.src = "french.jpg";
       }
-    });
+
+      // Sélection d'une langue
+      document.querySelectorAll(".lang-menu li").forEach((item) => {
+        item.addEventListener("click", (e) => {
+          const newLang = e.currentTarget.getAttribute("data-lang");
+          const langText =
+            e.currentTarget.querySelector(".lang-name").textContent;
+          this.state.updateLang(langText);
+          console.log("this.state.state.lang : " + this.state.state.lang);
+          langDiv.classList.remove("open");
+
+          selectedLangImg.classList.add("fade-out");
+
+          setTimeout(() => {
+            selectedLangImg.src = newLang;
+            selectedLangImg.classList.remove("fade-out");
+          }, 250);
+        });
+      });
+
+      // Fermer le menu quand on clique ailleurs
+      document.addEventListener("click", (e) => {
+        if (!langDiv.contains(e.target)) {
+          langDiv.classList.remove("open");
+        }
+      });
+    }
   }
 }
