1842f8996674dcddc2e020c56d04371319707258
Nathan Souchal <nsouchal@z3r12p2.42lyon.fr>
2025-03-14 13:37:30 +0100

    cleaned smart contract from debug

--- blockchain_module/contracts/StoreTournamentLogic.sol
+++ blockchain_module/contracts/StoreTournamentLogic.sol
@@ -6,8 +6,6 @@ contract StoreTournamentLogic is Initializable {
     address private _owner; 
     event   ContractInitialized(address owner);
     event   TournamentStored(uint tournamentId, address submittedBy);
-    event Debug(string message, uint value);
-    event DebugAddress(string message, address sender);
 
     struct  TournamentMatch {
         string  player1;
@@ -28,59 +26,53 @@ contract StoreTournamentLogic is Initializable {
 
     //Ici on ne peut pas directement stocker _round dans allTournamentsScores, car stocker un struct[] qui est en memory dans un mapping qui est en storage n'est pas possible
     function _storeRound(Round memory _round, uint _tournamentId) private {
-    emit Debug("Entree dans _storeRound", _round.roundID);
 
-    Round storage newRound = allTournamentsScores[_tournamentId].push();
-    newRound.roundID = _round.roundID;
+        Round storage newRound = allTournamentsScores[_tournamentId].push();
+        newRound.roundID = _round.roundID;
 
-    for (uint i = 0; i < _round.matches.length; i++) {
-        emit Debug("Ajout du match", i);
-        newRound.matches.push(_round.matches[i]);
+        for (uint i = 0; i < _round.matches.length; i++) {
+            newRound.matches.push(_round.matches[i]);
+        }
     }
-}
 
 
     function storeFullTournament(Round[] memory rounds, uint tournamentId) public {
-    emit Debug("Debut de storeFullTournament", tournamentId);
-    emit DebugAddress("Transaction envoyee par", msg.sender);
 
-    require(rounds.length > 0, "Tournament must have at least one round.");
-    emit Debug("Nombre de rounds recus", rounds.length);
+        require(rounds.length > 0, "Tournament must have at least one round.");
 
-    for (uint i = 0; i < rounds.length; i++) {
-        emit Debug("Traitement du round", rounds[i].roundID);
-        _storeRound(rounds[i], tournamentId);
+        for (uint i = 0; i < rounds.length; i++) {
+            _storeRound(rounds[i], tournamentId);
+        }
+
+        emit TournamentStored(tournamentId, msg.sender);
     }
-
-    emit TournamentStored(tournamentId, msg.sender);
-}
 
 
     function getTournament(uint tournamentId) public view 
-    returns(uint[] memory roundIDs, string[] memory player1s, string[] memory player2s, uint[] memory scorePlayer1s, uint[] memory scorePlayer2s) {
-        Round[] memory rounds = allTournamentsScores[tournamentId];
-        uint totalTournamentMatches = 0;
-        uint matchIndex = 0;
+        returns(uint[] memory roundIDs, string[] memory player1s, string[] memory player2s, uint[] memory scorePlayer1s, uint[] memory scorePlayer2s) {
+            Round[] memory rounds = allTournamentsScores[tournamentId];
+            uint totalTournamentMatches = 0;
+            uint matchIndex = 0;
 
-        for (uint i = 0; i < rounds.length; i++) {
-            totalTournamentMatches += rounds[i].matches.length;
-        }
-
-        roundIDs = new uint[](totalTournamentMatches);
-        player1s = new string[](totalTournamentMatches);
-        player2s = new string[](totalTournamentMatches);
-        scorePlayer1s = new uint[](totalTournamentMatches);
-        scorePlayer2s = new uint[](totalTournamentMatches);
-
-        for (uint i = 0; i < rounds.length; i++) {
-            for (uint j = 0; j < rounds[i].matches.length; j++) {
-                roundIDs[matchIndex] = rounds[i].roundID;
-                player1s[matchIndex] = rounds[i].matches[j].player1;
-                player2s[matchIndex] = rounds[i].matches[j].player2;
-                scorePlayer1s[matchIndex] = rounds[i].matches[j].scorePlayer1;
-                scorePlayer2s[matchIndex] = rounds[i].matches[j].scorePlayer2;
-                matchIndex++;
+            for (uint i = 0; i < rounds.length; i++) {
+                totalTournamentMatches += rounds[i].matches.length;
             }
-        }
+
+            roundIDs = new uint[](totalTournamentMatches);
+            player1s = new string[](totalTournamentMatches);
+            player2s = new string[](totalTournamentMatches);
+            scorePlayer1s = new uint[](totalTournamentMatches);
+            scorePlayer2s = new uint[](totalTournamentMatches);
+
+            for (uint i = 0; i < rounds.length; i++) {
+                for (uint j = 0; j < rounds[i].matches.length; j++) {
+                    roundIDs[matchIndex] = rounds[i].roundID;
+                    player1s[matchIndex] = rounds[i].matches[j].player1;
+                    player2s[matchIndex] = rounds[i].matches[j].player2;
+                    scorePlayer1s[matchIndex] = rounds[i].matches[j].scorePlayer1;
+                    scorePlayer2s[matchIndex] = rounds[i].matches[j].scorePlayer2;
+                    matchIndex++;
+                }
+            }
     }
 }
