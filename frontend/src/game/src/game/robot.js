bb768728826c6fb800a94849f5bc69126ef23328
cefue <cesar.fuentes@outlook.fr>
2025-04-04 14:29:39 +0200

    FIX: increased difficulty of easy and medium, reduce perceived size

--- frontend/src/game/src/game/robot.js
+++ frontend/src/game/src/game/robot.js
@@ -6,7 +6,7 @@ class Robot {
     this.paddle = paddle;
     this.size = size;
     this.deltaFactor = 30;
-    this.half_width = 3.12;
+    this.half_width = 2.8;
     this.target_x = 0;
     this.last_target_x = 0;
     this.timeSinceLastView = 0;
@@ -15,10 +15,10 @@ class Robot {
     this.offsetRange = [];
     switch (state.botDifficulty) {
       case 4:
-        this.offsetRange = [1, 5];
+        this.offsetRange = [0, 4];
         break;
       case 5:
-        this.offsetRange = [0, 3.5];
+        this.offsetRange = [0, 2.5];
         break;
       case 6:
         this.offsetRange = [0, 0];
